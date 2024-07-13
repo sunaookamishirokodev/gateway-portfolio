1d00c7d5648fbcd484cd55b20e678e7e3e9c3558
Furinaaaa <140150717+sunaookamishirokodev@users.noreply.github.com>
2024-07-13 09:07:40 +0700

    feat: discord presence

---
+++ index.ts
@@ -0,0 +1,110 @@
+import { ActivityType, Client, Presence } from "discord.js";
+import { Server, Socket } from "socket.io";
+
+let _socket: Socket | undefined;
+
+const client = new Client({
+    intents: ["Guilds", "GuildMembers", "GuildPresences"],
+});
+
+const io = new Server({
+    cors: {
+        origin: [
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "https://me.shirokodev.site",
+            "https://me-sunaookamishirokodev.vercel.app/",
+            "https://me-sepia-ten.vercel.app/",
+            "https://me-git-main-sunaookamishirokodev.vercel.app/",
+        ],
+        credentials: false,
+    },
+});
+
+const userId = process.env.USER_ID as string;
+
+function parsePresenceData(presence: Presence) {
+    const customStatus = presence.activities.find((act) => act.type === ActivityType.Custom);
+    const activity = presence.activities.find((act) => act.type !== ActivityType.Custom);
+    return {
+        user: {
+            username: presence.user?.username,
+            avatar: presence.user?.displayAvatarURL(),
+            displayName: presence.user?.displayName,
+            banner: presence.user?.bannerURL(),
+            status: {
+                type: presence.status,
+                devices: presence.clientStatus,
+            },
+        },
+        customStatus: customStatus && {
+            state: customStatus.state,
+            emoji: {
+                name: customStatus.emoji?.name,
+                url: customStatus.emoji?.imageURL(),
+            },
+        },
+        activity: activity && {
+            name: activity.name,
+            url: activity.url,
+            details: activity.details,
+            state: activity.state,
+            assets: activity.assets && {
+                smallText: activity.assets.smallText,
+                smallImage: activity.assets.smallImageURL(),
+                largeText: activity.assets.largeText,
+                largeImage: activity.assets.largeImageURL(),
+            },
+            timestamps: {
+                start: activity.timestamps?.start && Number(activity.timestamps.start),
+                end: activity.timestamps?.end && Number(activity.timestamps.start),
+            },
+            createdTimestamp: activity.createdTimestamp,
+        },
+    };
+}
+
+io.on("connection", (socket) => {
+    _socket = socket;
+    console.log("User connected");
+    socket.on("getPresence", () => {
+        const g = client.guilds.cache.find((g) => g.members.cache.has(userId));
+        if (!g) {
+            socket.emit(
+                "error",
+                JSON.stringify({
+                    error: "USER_NOT_FOUND",
+                    data: userId,
+                    msg: "Wrong user id or bot not ready",
+                })
+            );
+            return socket.disconnect(true);
+        }
+        const member = g.members.cache.get(userId);
+        if (!member) return;
+
+        member.user.fetch(true).then(() => {
+            if (member.presence) {
+                socket.emit("updatePresence", JSON.stringify(parsePresenceData(member.presence)));
+            }
+        });
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected", userId);
+    });
+});
+
+client.on("presenceUpdate", (_, newP) => {
+    console.log("Update");
+    if (newP.userId === userId) {
+        _socket?.emit("updatePresence", JSON.stringify(parsePresenceData(newP)));
+    }
+});
+
+client.on("ready", (bot) => {
+    console.log("Login as", bot.user.username);
+    io.listen(4000);
+});
+
+client.login(process.env.TOKEN);
